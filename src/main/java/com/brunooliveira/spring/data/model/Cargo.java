72f82e2ae35375ae3b68f26c141ce50c174c83c7
BRUNOOLIVEIRA13 <bruno.henrique0liveir4@gmail.com>
2021-05-29 21:11:44 -0300

    Created database. :tada:

---
+++ src/main/java/com/brunooliveira/spring/data/model/Cargo.java
@@ -0,0 +1,36 @@
+package com.brunooliveira.spring.data.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_cargos")
+public class Cargo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+
+    public Cargo() {
+    }
+
+    public Cargo(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
